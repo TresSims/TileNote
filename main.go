b9ee1b2b5cbc95f88acda73db59bc9a6665be47d
Tres Sims <tres.sims95@gmail.com>
2021-12-02 23:13:28 -0500

    Initial Commit

---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"embed"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"os/signal"
+	"runtime"
+
+	"github.com/zserge/lorca"
+)
+
+//go:embed TileNoteSrc/out/_next
+var fs embed.FS
+
+func main() {
+	args := []string{}
+	if runtime.GOOS == "linux" {
+		args = append(args, "--class=Lorca")
+	}
+	ui, err := lorca.New("", "", 480, 320, args...)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer ui.Close()
+
+	ui.Bind("start", func() {
+		log.Println("UI is ready")
+	})
+
+	ln, err := net.Listen("tcp", "127.0.0.1:0")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer ln.Close()
+	go http.Serve(ln, http.FileServer(http.FS(fs)))
+	ui.Load(fmt.Sprintf("http://%s/TileNoteSrc/out/_next", ln.Addr()))
+
+	sigc := make(chan os.Signal)
+	signal.Notify(sigc, os.Interrupt)
+	select {
+	case <-sigc:
+	case <-ui.Done():
+	}
+}
